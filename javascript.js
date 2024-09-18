a55fca508f2573c7172e4d905eb5c036e0cde78a
Mustafa Abubakar <mx_1200@hotmail.com>
2024-09-19 03:00:18 +0400

    working on second page

---
+++ javascript.js
@@ -0,0 +1,8 @@
+
+document.querySelector('.options-container').addEventListener('click', e => {
+    if(e.target.className === "options-container") return;
+    const subject = e.target.closest(".option").value;
+    localStorage.setItem("subject", subject);
+    window.location.href = "./subject.html";
+
+})
