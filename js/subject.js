33726d62396ad504c1b12100a09883b5ff296a85
mustafa abdishakur <mx_1200@hotmail.com>
2024-09-25 10:51:19 +0400

    render questions

--- js/subject.js
+++ js/subject.js
@@ -9,13 +9,16 @@ const DOM = {
 const eventDOM = {
     optionsContainer: document.querySelector('.options-container'),
     submitBtn: document.querySelector('.submit-btn'),
+    progressBar: document.querySelector('.progress-bar'),
 }
 const state = {
     subject: '',
-    subjectInfo:{},
+    subjectInfo: {},
     questionNumber: 1,
     score: 0,
-    selectedOption: ""
+    selectedOption: "",
+    submitAnswer: false,
+    progress: 10
 
 }
 window.onload = () => {
@@ -33,32 +36,34 @@ window.onload = () => {
     for (const [key, value] of Object.entries(DOM)) {
         value.innerHTML = '';
     }
-    const quiz = data.quizzes.find( quiz => quiz.title === subject);
+    const quiz = data.quizzes.find(quiz => quiz.title === subject);
 
     // console.log(quiz)
     state.subjectInfo = quiz;
-
-    DOM.subjectLogo.src = quiz.icon; 
-    DOM.subjectTitle.textContent = quiz.title;
+    renderQuestion();
+}
+const renderQuestion = () => {
+    DOM.subjectLogo.src = state.subjectInfo.icon;
+    DOM.subjectTitle.textContent = state.subjectInfo.title;
     DOM.questionNumber.textContent = `Question ${state.questionNumber} of 10`;
-    DOM.question.textContent = quiz.questions[state.questionNumber - 1].question;
-    DOM.options.forEach((option,index) => {
-     option.textContent = quiz.questions[state.questionNumber - 1].options[index];
+    DOM.question.textContent = state.subjectInfo.questions[state.questionNumber - 1].question;
+    DOM.options.forEach((option, index) => {
+        option.textContent = state.subjectInfo.questions[state.questionNumber - 1].options[index];
     });
-
-    
-
 }
 // https://www.quora.com/How-do-I-modify-the-URL-without-reloading-the-page-using-JavaScript
 
-DOM.options.forEach( option => {
+DOM.options.forEach(option => {
     option.addEventListener('click', e => {
-        state.selectedOption = e.target.closest(".selected").textContent;
-        // Highlight selected option
+        if (state.submitAnswer == true) return;
+
+        state.selectedOption = e.target.closest(".select").textContent;
+
         Array.from(eventDOM.optionsContainer.children).forEach(el => {
             el.classList.remove("active");
             el.children[0].classList.remove("active2");
         })
+        // Highlight selected option
         e.target.closest('.option').classList.add("active");
         e.target.parentElement.children[0].classList.add("active2");
 
@@ -66,18 +71,53 @@ DOM.options.forEach( option => {
 })
 
 eventDOM.submitBtn.addEventListener("click", (e) => {
-    if (state.selectedOption === "") return;
-    if (state.selectedOption === state.subjectInfo.questions[state.questionNumber - 1].answer) {
-        document.querySelector('.active').classList.add("correct");
-        document.querySelector('.active2').classList.add("correct2"); 
+    if (e.target.textContent === "Submit Answer") {
+        if (state.selectedOption === "") return;
+        state.submitAnswer = true;
 
+        if (state.selectedOption === state.subjectInfo.questions[state.questionNumber - 1].answer) {
+            // correct answer
+            state.score = state.score + 1;
+            document.querySelector('.active').classList.add("correct");
+            document.querySelector('.active2').classList.add("correct2");
+            document.querySelector('.active').children[2].style.display = "initial";
+            // console.log(document.querySelector('.active').children[2])
+        } else {
+            // wrong answer
+            document.querySelector('.active').classList.add("false");
+            document.querySelector('.active2').classList.add("false2");
+            document.querySelector('.active').children[3].style.display = "initial";
+            DOM.options.forEach(el => {
+                if (el.textContent === state.subjectInfo.questions[state.questionNumber - 1].answer) {
+                    el.parentElement.children[2].style.display = "initial";
+                }
+            })
+        }
+
+
+        e.target.textContent = "Next Question";
     } else {
-        document.querySelector('.active').classList.add("false");
-        document.querySelector('.active2').classList.add("false2");
-         DOM.options.forEach(el => {
-            if( el.textContent === state.subjectInfo.questions[state.questionNumber - 1].answer) {
-                el.parentElement.classList.add("correct");
-            }
+        //NEXT QUESTION
+        // Reset state and DOM
+        state.questionNumber = state.questionNumber + 1;
+        state.selectedOption = "";
+        state.submitAnswer = false;
+        e.target.textContent = "Submit Answer";
+        DOM.options.forEach(el => {
+            el.parentElement.classList.remove("active");
+            el.parentElement.classList.remove("correct");
+            el.parentElement.classList.remove("false");
+            el.parentElement.children[0].classList.remove("active2");
+            el.parentElement.children[0].classList.remove("correct2");
+            el.parentElement.children[0].classList.remove("false2");
+            el.parentElement.children[2].style.display = "none";
+            el.parentElement.children[3].style.display = "none";
         })
+        // Increase percentage bar
+        state.progress = state.progress + 10;
+        eventDOM.progressBar.style.width = `${state.progress}%`;
+
+        //Render new question
+        renderQuestion();
     }
 })
